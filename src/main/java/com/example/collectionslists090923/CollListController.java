3b04a76c215d340e3706a1b803b418e7ba47165b
air <kobychev00@bk.ru>
2023-09-09 13:03:02 +0300

    09:09:23 13:02

---
+++ src/main/java/com/example/collectionslists090923/CollListController.java
@@ -0,0 +1,39 @@
+package com.example.collectionslists090923;
+
+import com.example.collectionslists090923.model.Employee;
+import com.example.collectionslists090923.service.EmployeeService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("employee")
+public class CollListController {
+
+    private final EmployeeService employeeService;
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+
+    @GetMapping(path = "/add")
+    public Employee add(@RequestParam String firstName, @RequestParam String lastName) {
+        return employeeService.add(firstName, lastName);
+    }
+
+    public CollListController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+
+    @GetMapping
+    public List<Employee> getAll() {
+        return employeeService.getAll();
+    }
+}
+
---
+++ src/main/java/com/example/collectionslists090923/service/EmployeeService.java
@@ -0,0 +1,31 @@
+package com.example.collectionslists090923.service;
+
+
+import com.example.collectionslists090923.model.Employee;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class EmployeeService {
+    private final List<Employee> employees = new ArrayList<>();
+
+
+
+    public Employee add(String firstName, String lastName) {
+        Employee newEmployee = new Employee(firstName, lastName);
+        employees.add(newEmployee);
+        return newEmployee;
+    }
+    public Employee remove(String firstName, String lastName) {
+        return null;
+    }
+    public Employee get(String firstName, String lastName) {
+        return null;
+    }
+
+    public List<Employee> getAll() {
+        return employees;
+    }
+}
